fe89f12fc995bb2e6b580ff10a66819a4034a730
Piotr Olejarz <olejarz-piotr@o2.pl>
2021-10-28 22:56:42 +0200

    last commit

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const router = express.Router();
+const router = express.Router()
 
-const { getAllProducts, getAllProductsStatic } = require('../controllers/products')
+const {
+  getAllProducts,
+  getAllProductsStatic,
+} = require('../controllers/products')
 
-router.route('/').get(getAllProducts);
-router.route('/static').get(getAllProductsStatic);
+router.route('/').get(getAllProducts)
+router.route('/static').get(getAllProductsStatic)
 
-module.exports = router;
+module.exports = router
